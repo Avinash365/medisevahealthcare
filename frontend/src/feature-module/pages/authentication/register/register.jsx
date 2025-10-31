fca5a9fd26f668fd9e2cd6104045e20dae0d7a28
amitbtechcse-sketch <amit.btechcse@gmail.com>
2025-10-31 09:30:59 +0530

    Update register.jsx

--- frontend/src/feature-module/pages/authentication/register/register.jsx
+++ frontend/src/feature-module/pages/authentication/register/register.jsx
@@ -239,7 +239,7 @@ const Register = () => {
                     </div>
                   </div>
                   <div className="my-4 d-flex justify-content-center align-items-center copyright-text">
-                    <p>Copyright © 2025 DreamsPOS</p>
+                    <p>Copyright © 2025 MediSevaHealthCare</p>
                   </div>
                 </div>
               </form>
